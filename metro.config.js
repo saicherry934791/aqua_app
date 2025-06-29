a2705eb0ce8e9ef273764b5aebf528ef6b6d4882
saicherry934791 <162865505+saicherry934791@users.noreply.github.com>
2025-06-29 17:47:58 +0530

    Fix Metro bundler native module import errors

--- metro.config.js
+++ metro.config.js
@@ -12,4 +12,11 @@ config.resolver.alias = {
 // Platform-specific extensions
 config.resolver.platforms = ['web', 'ios', 'android', 'native'];
 
+// Block native-only modules from being imported on web
+config.resolver.blockList = config.resolver.blockList || [];
+config.resolver.blockList.push(
+  /node_modules\/react-native-maps\/lib\/MapMarkerNativeComponent\.js/,
+  /node_modules\/react-native\/Libraries\/Utilities\/codegenNativeCommands\.js/
+);
+
 module.exports = withNativeWind(config, { input: './global.css' });
